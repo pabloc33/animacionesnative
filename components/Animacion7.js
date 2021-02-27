9e8aa7806ebf6ed5e91a036f4a619d9d0b62def8
pabloc33 <pablo.contino33@gmail.com>
2021-02-27 00:22:15 -0300

    Primera Actulización

---
+++ components/Animacion7.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, Animated} from 'react-native';
+
+const Animacion7 = () => {
+  const [animacion1] = useState(new Animated.Value(0));
+  const [animacion2] = useState(new Animated.Value(1));
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(animacion1, {
+        toValue: 300,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(animacion2, {
+        toValue: 10,
+        useNativeDriver: true,
+      }),
+      Animated.spring(animacion2, {
+        toValue: 1,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animacion1, {
+        toValue: 600,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  const estiloAnimacion = {
+    transform: [{translateY: animacion1}, {scale: animacion2}],
+  };
+
+  return (
+    <View>
+      <Animated.View style={[styles.caja, estiloAnimacion]}></Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  caja: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'cornflowerblue',
+  },
+});
+
+export default Animacion7;
